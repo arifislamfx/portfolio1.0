bc04edda3c1323deff2e71040c9247c306f7367f
Ariful Islam <iariful.info@gmail.com>
2022-05-08 02:29:03 +0600

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
-
+import { Navbar, Welcome, Footer, Services } from './components';
 function App() {
  
 
   return (
-    <div className="App">
-        <h1 className="text-3xl text-center font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Footer />
     </div>
   )
 }
